1ce9437db949c2a1df4cf64cce178c74e4a189da
93chidiebere <vincent.christopher.189736@unn.edu.ng>
2023-10-23 18:17:17 -0700

    removed HR files

--- api.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, json, request, jsonify
-import pickle
-
-app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-
-@app.route('/')
-def home():
-    return 'Welcome to HR Solution API'
